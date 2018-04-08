9e612d3cbf299a76da6370aa882affafa5df565c
Zachary Swoveland <ZSwoveland@my.gcu.edu>
2018-04-08 11:26:08 -0700

    My Java File for Exercise 1 - Zachary Swoveland

---
+++ Topic_1/Welcome.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Welcome 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("My name is Zachary.");
+		System.out.println("My favorite color is purple.");
+		System.out.println("If I won the lottery, I would pay for college and use the rest for my family.");
+	}
+
+}
